0a92b1c11f11587009c6920b51618f3126d7a30b
leongpeng <leongpeng.tou@gmail.com>
2017-04-09 14:48:55 +0800

    remove module setting

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'version' => '0.0.1',
     'drivers' => [
         'shadowsocks' => Rocketscoket\Drivers\Shadowsocks::class
     ]
--- rocketsocket.php
+++ rocketsocket.php
@@ -2,8 +2,6 @@
 
 !defined('WHMCS') && die('This file cannot be accessed directly');
 
-use Rocketsocket\ModuleSetting;
-
 require __DIR__ . '/vendor/autoload.php';
 
 function rocketsocket_MetaData()
@@ -16,5 +14,35 @@ function rocketsocket_MetaData()
 
 function rocketsocket_ConfigOptions()
 {
-    return array_merge(ModuleSetting::config(), ModuleSetting::packages());
+    $options = [];
+    $drivers = config('drivers');
+    foreach ($drivers as $driver => $class) {
+        $options[$driver] = $driver;
+    }
+
+    return [
+        'Shadowsocks' => [
+            'Type' => 'dropdown',
+            'Options' => $options
+        ],
+        'Database' => [
+            'Type' => 'text',
+            'Size' => '25'
+        ],
+        'Bandwidth' => [
+            'Type' => 'text',
+            'Size' => '25'
+        ],
+        // should be override, configurable, In Database or File ?
+        'Servers' => [
+            'Type' => 'textarea',
+            'Rows' => 5,
+            'Cols' => 35
+        ]
+    ];
+}
+
+function rocketsocket_CreateAccount(array $parameters)
+{
+    //
 }
