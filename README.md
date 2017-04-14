ad76753995993bcd4cc7cc5c5e254f297936ac9b
leongpeng <leongpeng.tou@gmail.com>
2017-04-14 22:20:08 +0800

    add sql

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # rocketsocket
 
+原谅我英语差，还想装个逼。
+
 Work is in progress. don't use in your production.
 
 Shadowsocks For WHMCS Module. (Basic version.)
@@ -12,13 +14,15 @@ first:
 
 then:
 
-    create a new directory in your whmcs installation directory.
+create a new directory in your whmcs installation directory.
 
-    example: `mkdir /app/web/whmcs/modules/servers/rocketsocket`
+example: `mkdir /app/web/whmcs/modules/servers/rocketsocket`
+
+import `sql/init.sql` to your shadowsocks database.
 
 finally:
 
-    upload rocketsocket files to `/app/web/whmcs/modules/servers/rocketsocket`
+upload rocketsocket files to `/app/web/whmcs/modules/servers/rocketsocket`
 
 enjoy it.
 
---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `passwd` varchar(16) NOT NULL,
+  `t` int(11) NOT NULL DEFAULT '0',
+  `u` bigint(20) NOT NULL,
+  `d` bigint(20) NOT NULL,
+  `transfer_enable` bigint(20) NOT NULL,
+  `port` int(5) NOT NULL,
+  `protocol` varchar(128) NOT NULL DEFAULT 'origin',
+  `obfs` varchar(128) NOT NULL DEFAULT 'plain',
+  `switch` tinyint(1) NOT NULL DEFAULT '1',
+  `enable` tinyint(1) NOT NULL DEFAULT '1',
+  `method` varchar(64) NOT NULL DEFAULT 'chacha20',
+  `service_id` int(10) unsigned NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `service_id` (`service_id`),
+  UNIQUE KEY `port` (`port`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `recycle_bin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `port` int(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
